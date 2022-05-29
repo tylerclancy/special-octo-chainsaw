c7cc12df59ff916f6c5311ae5c15e969baa4bab7
Tyler <tylerclancy030@gmail.com>
2022-05-29 14:55:25 -0500

    Bug fix - card not reacting to clicks

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,10 +5,10 @@ function Card({ image, selected, onClick }) {
       <div className={selected && 'selected'}>
         <img src={image} alt="" className='card-face' />
 
-        <img className='card-back' src="/assets/card-back-black.png" alt=""/>
+        <img className='card-back' src="/assets/card-back-black.png" alt="" onClick={onClick}/>
       </div>
     </div>
-  )
+  );
 }
 
 export default Card;
